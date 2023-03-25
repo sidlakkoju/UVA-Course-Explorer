9572dd478815326bdeb2df920b72a9ac46b9c913
Saahith Janapati <itzsaahith01@gmail.com>
2023-03-25 16:16:33 -0400

    https://www.youtube.com/watch?v=iXzk9H3hIhc

--- visualization/test_app.py
+++ visualization/test_app.py
@@ -9,13 +9,24 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     # Generate some random data
+    # Generate some random data
+    # Generate some random data
     x, y, z = np.random.multivariate_normal([0,0,0], np.eye(3), 500).transpose()
 
     # Create a 3D scatter plot
     trace = go.Scatter3d(x=x, y=y, z=z, mode='markers', marker=dict(size=5, color=z, colorscale='Viridis', opacity=0.8))
 
+    # Set the background color and font color
+    bgcolor = '#1f1f1f'
+    fontcolor = '#FFFFFF'
+
     # Create a layout for the plot
-    layout = go.Layout(scene=dict(xaxis=dict(title='X'), yaxis=dict(title='Y'), zaxis=dict(title='Z')), width=800, height=700)
+    layout = go.Layout(scene=dict(xaxis=dict(title='X', backgroundcolor=bgcolor, gridcolor=fontcolor, showbackground=True, zerolinecolor=fontcolor, showgrid=False),
+                                yaxis=dict(title='Y', backgroundcolor=bgcolor, gridcolor=fontcolor, showbackground=True, zerolinecolor=fontcolor, showgrid=False),
+                                zaxis=dict(title='Z', backgroundcolor=bgcolor, gridcolor=fontcolor, showbackground=True, zerolinecolor=fontcolor), showgrid=False),
+                    paper_bgcolor=bgcolor,
+                    font=dict(color=fontcolor),
+                    width=800, height=700)
 
     # Create a figure and add the trace and layout
     fig = go.Figure(data=[trace], layout=layout)
