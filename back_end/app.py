597ea6a3ecab3b046be5f05a91a8a1fef1def7a1
Saahith Janapati <itzsaahith01@gmail.com>
2023-03-26 01:24:59 -0400

    updates for new detailed info rendering

--- back_end/app.py
+++ back_end/app.py
@@ -8,6 +8,9 @@ import numpy as np
 from semantic_search import *
 import threading
 import requests
+import json
+from datetime import datetime
+
 
 openai.api_key = openai_key
 
@@ -20,9 +23,7 @@ test_return_array = [
 
 
 detailed_info = {}
-
 app = Flask(__name__)
-
 
 
 # class for query builder?
@@ -36,11 +37,9 @@ def get_base_url(year=2023, term="spring"):
 
 
 
-
 def get_detailed_info_url(catalog_nbr, subject, year=2023, term="spring"):
     base_url = get_base_url(year, term)
-    return f"{base_url}&subject={subject}&catlog_nbr={catalog_nbr}"
-
+    return f"{base_url}&catlog_nbr={catalog_nbr}&subject={subject}"
 
 
 # Test route
@@ -49,32 +48,101 @@ def get_members():
     return {'members': ['John', 'Paul', 'George', 'Ringo']}
 
 
+# time_string = "11.00.00.000000-05:00"
+def parse_time(time_string, time_format = '%H.%M.%S.%f%z'):
+    parsed_time = datetime.strptime(time_string, time_format)
+    hours_minutes = f"{parsed_time.hour:02d}:{parsed_time.minute:02d}"
+    return  hours_minutes
+
 
 
 def get_detailed_info(json_results):
     global detailed_info
-    for i in range(len(json_results)):
-        catalog_nbr = json_results[i]['catalog_nbr']
-        subject = json_results[i]['subject']
-
-
-
-        detailed_info_url = get_detailed_info_url(catalog_nbr, subject)
+    json_results = json.loads(json_results)
+    for i in range(len(json_results)):      # for each search result
+        catalog_nbr = json_results[i]['catalog_number']
+        subject_mnemonic = json_results[i]['mnemonic']
+        detailed_info_url = get_detailed_info_url(catalog_nbr, subject_mnemonic)
         r = requests.get(detailed_info_url)
         array = r.json()
+        detailed_info_array = []
+        print("Getting data for " + subject_mnemonic + " " + str(catalog_nbr))
 
-        print(array)
+        for session in array:       # for each meeting session of the class we're looking at
+            session_data = {}
+            try:
+                session_data['instructor'] = session['instructors'][0]['name']
+            except:
+                session_data['instructor'] = "N/A"
+
+            try:
+                session_data['location'] = session['meetings'][0]['facility_descr']
+            except:
+                session_data['location'] = "N/A"
+            
+            try:
+                session_data['enrollment_capacity'] = session['class_capacity']
+            except:
+                session_data['enrollment_capacity'] = "N/A"
+            
+            try:
+                session_data['current_enrolled'] = session['enrollment_total']
+            except:
+                session_data['current_enrolled'] = "N/A"
+            
+            try:
+                session_data['waitlist_capacity'] = session['wait_cap']
+            except:
+                session_data['waitlist_capacity'] = "N/A"
+
+            try:
+                session_data['current_waitlisted'] = session['wait_tot']
+            except:
+                session_data['current_waitlisted'] = "N/A"
+
+            try:
+                session_data['days'] = session['meetings'][0]['days']
+            except:
+                session_data['days'] = "N/A"
+
+            try:
+                session_data['start_time'] = parse_time(session['meetings'][0]['start_time'].strip())
+                session_data['end_time'] = parse_time(session['meetings'][0]['end_time'].strip())
+            except:
+                session_data['start_time'] = "N/A"
+                session_data['end_time'] = "N/A"
+
+            detailed_info_array.append(session_data)
+
+        detailed_info[(subject_mnemonic, catalog_nbr)] = detailed_info_array
+    
+    print(detailed_info)
+    
+
+
 
 
 @app.route('/search', methods=['POST'])
 def search():
     search_input = request.json['searchInput']
     json_results = get_top_results_json(search_input, n=10)
-
     t = threading.Thread(target=get_detailed_info, args=(json_results,))
     t.start()
-    t.join()
     return json_results
+
+
+
+
+@app.route('/detailed_info', methods=['POST'])
+def get_detailed_info():
+    catalog_nbr = request.json['catalog_number']
+    subject_mnemonic = request.json['mnemonic']
+    if (subject_mnemonic, catalog_nbr) in detailed_info:
+        return detailed_info[(subject_mnemonic, catalog_nbr)]
+    else:
+        while (subject_mnemonic, catalog_nbr) not in detailed_info:
+            time.sleep(0.5)
+        return detailed_info[(subject_mnemonic, catalog_nbr)]
 
 
 if __name__ == '__main__':
--- back_end/semantic_search.py
+++ back_end/semantic_search.py
@@ -26,17 +26,19 @@ def get_top_results_df(query, n=10):
 def get_top_results_json(query, n=10):
    df = get_top_results_df(query, n)
 #    print(df)
-   df.drop(columns=['ada_embedding', 'similarities', 'n_tokens', 'acad_org', 'class_nbr', 'subject'], inplace=True)
+   df.drop(columns=['ada_embedding', 'n_tokens', 'acad_org', 'class_nbr'], inplace=True)
    df = df.rename(columns={'descr': 'name',
                            'acad_career_descr' : 'level',
                            'catalog_nbr': 'catalog_number',
                            'class_nbr': 'class_number',
-                           'subject_descr': 'subject'})
+                           'subject_descr': 'subject',
+                           'subject': 'mnemonic',
+                           })
    
 
-   for c in df.columns:
-       print(df.iloc[0][c])
-   print(df.to_json(orient='records'))
+#    for c in df.columns:
+#        print(df.iloc[0][c])
+#    print(df.to_json(orient='records'))
    return df.to_json(orient='records')
 
 
