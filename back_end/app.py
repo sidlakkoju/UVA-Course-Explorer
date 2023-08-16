0d6cc1651c4af316b4103b2ae066804c14c3c453
Saahith Janapati <saahithjanapati@saahiths-mbp.lan>
2023-08-16 12:39:50 -0400

    numpy-ify the search stuff

--- back_end/app.py
+++ back_end/app.py
@@ -69,8 +69,6 @@ def populate_detailed_info(json_results):
         print("Getting data for " + subject_mnemonic + " " + str(catalog_nbr))
 
         for session in array:       # for each meeting session of the class we're looking at
-
-
             # need to filter out results cuz SIS API is sometimes weird
             if str(session['subject']) != subject_mnemonic or str(session['catalog_nbr']) != catalog_nbr:
                 continue
@@ -141,7 +139,6 @@ def search():
 
 @app.route('/detailed_info', methods=['POST'])
 def get_detailed_info():
-
     '''
     print(detailed_info.keys())
     print("Getting detailed info")
--- back_end/semantic_search.py
+++ back_end/semantic_search.py
@@ -3,9 +3,30 @@ import pandas as pd
 from config import openai_key
 import openai
 import numpy as np
+import pickle
+import json
 
 
 openai.api_key = openai_key
+"""
+{
+   "catalog_number":1040,
+   "mnemonic":"RELG",
+   "subject":"General Religion",
+   "name":"Introduction to Eastern Religious Traditions",
+   "credits":"3",
+   "level":"Undergraduate",
+   "description":"Introduces various aspects of the religious traditions of India, China, and Japan.",
+   "similarity_score":0.835}
+"""
+
+
+with open('embedding_matrix.pkl', 'rb') as embedding_file:
+    embedding_matrix = pickle.load(embedding_file)
+
+# Load data dictionary from pickle file
+with open('course_data_dict.pkl', 'rb') as data_dict_file:
+    course_data_dict = pickle.load(data_dict_file)
 
 
 def get_embedding(text, model="text-embedding-ada-002"):
@@ -24,9 +45,29 @@ def get_top_results_df(query, n=10):
    return res
 
 
+def cosine_similarity_search(query_vector, embedding_matrix):
+    similarities = np.dot(embedding_matrix, query_vector) / (np.linalg.norm(embedding_matrix, axis=1) * np.linalg.norm(query_vector))
+    return similarities
+
+
 def get_top_results_json(query, n=10):
+   query_vector = get_embedding(query, model='text-embedding-ada-002')
+   similarities = cosine_similarity_search(query_vector, embedding_matrix)
+
+   top_n_indices = np.argsort(similarities)[::-1][:n]
+   top_n_data = [course_data_dict[index] for index in top_n_indices]
+   
+   # add the similarity scores as values in the dictionaries
+   for i in range(n):
+      matrix_index = top_n_indices[i]
+      top_n_data[i]["similarity_score"] = similarities[matrix_index]
+   
+   return json.dumps(top_n_data)
+
+
+def get_top_results_json_old(query, n=10):
    df = get_top_results_df(query, n)
-#    print(df)
+   #    print(df)
    df.drop(columns=['ada_embedding', 'n_tokens', 'acad_org', 'class_nbr'], inplace=True)
    df = df.rename(columns={'descr': 'name',
                            'acad_career_descr' : 'level',
@@ -36,20 +77,3 @@ def get_top_results_json(query, n=10):
                            'subject': 'mnemonic',
                            'similarities': 'similarity_score',
                            'units': 'credits'})
-   
-
-#    for c in df.columns:
-#        print(df.iloc[0][c])
-#    print(df.to_json(orient='records'))
-   return df.to_json(orient='records')
-
-
-
-
-
-# catalog number        -->  catalog_number
-# department   --> subject
-# course title --> name
-# course description  --> description
-# undergraduate or graduate --> level
-# credits --> credits
